f6a6178335a078d188386875a66c622ed652c10c
Aryan Patil <aryanpatil@gmail.com>
2026-09-02 19:20:35 +0530

    Change horse galloping direction to Right-to-Left with trailing dust cloud

--- frontend/src/components/HorseGallopAnimation.jsx
+++ frontend/src/components/HorseGallopAnimation.jsx
@@ -4,8 +4,8 @@ export const HorseGallopAnimation = () => {
   return (
     <div className="track-animation-container">
       <div className="running-horse-icon" title="RACEHUB Live Track Line">
-        <span style={{ opacity: 0.75, fontSize: '1.4rem', marginRight: '2px', display: 'inline-block' }}>💨</span>
         <span>🏇</span>
+        <span style={{ opacity: 0.75, fontSize: '1.4rem', marginLeft: '2px', display: 'inline-block' }}>💨</span>
       </div>
     </div>
   );
